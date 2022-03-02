74ab15fec22a8c5cb6150bb4f11c6785641bcc61
rpg225 <rpg225@gmail.com>
2022-03-02 14:09:24 -0500

    Hello World!

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar />
+  <Footer />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="footer" >
+
+   </footer>
+  </template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+  position: absolute;
+    bottom: 0;
+    width: 120%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #000;
+}
+</style>
